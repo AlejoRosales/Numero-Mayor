754c55aee5e941fe379e08faf767bf9ac341567b
Alejandro <alejo.rosales95@gmail.com>
2016-09-25 16:07:23 -0400

    Update and rename main-Ejercicio1.cpp to Ejercicio1.cpp

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -5,19 +5,19 @@
 int main(int argc, char *argv[]) {
 	int A, B;
 	
-	printf ("Ejercicio 1:\nCrear un programa que lea dos numeros y que indique el mayor de los dos.");
+	printf ("Ejercicio 1:\nCrear un Programa que lea dos numeros y que indique el MAYOR de los dos.");
 	printf ("\n------------------------------------------------------------------------------");
 	
-	printf ("\n\nEscribe el primer numero :");
+	printf ("\n\nEscribe el Primer Numero :");
 	scanf ("%d",&A);
 	
-	printf ("\nEscribe el segundo numero :");
+	printf ("\nEscribe el Segundo Numero :");
 	scanf ("%d",&B);
 	
 	if (A > B){
-		printf ("\n\nEl Numero Mayor es :%d",A);
+		printf ("\n\nEl Numero MAYOR es :%d",A);
 	}else{
-		printf ("\n\nEl Numero Mayor es :%d",B);
+		printf ("\n\nEl Numero MAYOR es :%d",B);
 	}
 	
 	getch();
